cba2a5de731db33f1f111b384b037ca6aeb0112c
Mahadi Islam <phenomenalthe65@gmail.com>
2026-01-22 01:42:06 +0600

    Delete 209.R

--- 209.R
+++
@@ -1,7 +0,0 @@
-install.packages("readxl")
-library(readxl)
-project_distribution.df<-read_excel("C:\Users\ASUS\OneDrive\Documents\R programming\Project_Distribution.xlsx",sheet = "Data")
-
-class(project_distribution.df)
-str(project_distribution.df)
-head(project_distribution.df)
